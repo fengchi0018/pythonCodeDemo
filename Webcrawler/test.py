ad7a686cb0d26c0591ae4670fa44221073df1bd8
fengchi0018 <2652892843@qq.com>
2022-03-31 17:17:42 +0800

    0331

--- Webcrawler/test.py
+++ Webcrawler/test.py
@@ -20,33 +20,54 @@ headers2 = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
     'Referer': 'http://182.131.1.150:9090/hydrologicDataTwo.html',
 }
-
 url = 'http://182.131.1.150:9090/hydrologicDataTwo.html'
 info_url = 'http://182.131.1.150:9090/WaterInfo/getWaterInfoData'
-# 获取地区信息
-response = requests.get(url, headers=headers)
-response.encoding = 'utf-8'
-html = response.text
-data = re.findall("var orgs =(.*)]\r\n", html, re.S)[0]
-# split()返回list
-datas = data.replace('[', '').strip().replace(' ', '').split()
-list_data = []
-for data in datas:
-    cases = {}
-    code = data[data.find('"', 1, 8) + 1:data.find('n', 1, len(data)) - 2]
-    name = data[data.find('name', 1, len(data)) + 6:len(data) - 3]
-    cases = {"code": code, "name": name}
-    # 字典加入list
-    list_data.append(cases)
-# 地区循环
-for data in list_data:
-    # 创建及编写表头文件
-    file_title = data['name']
-    with open(f'file/{file_title}.csv', mode='a', encoding='utf-8-sig', newline='') as f:
-        print("新建",file_title,"成功")
+
+
+def get_region():
+    # 获取地区信息
+    response = requests.get(url, headers=headers)
+    response.encoding = 'utf-8'
+    html = response.text
+    data = re.findall("var orgs =(.*)]\r\n", html, re.S)[0]
+    # split()返回list
+    datas = data.replace('[', '').strip().replace(' ', '').split()
+    list_data = []
+    for data in datas:
+        cases = {}
+        code = data[data.find('"', 1, 8) + 1:data.find('n', 1, len(data)) - 2]
+        name = data[data.find('name', 1, len(data)) + 6:len(data) - 3]
+        cases = {"code": code, "name": name}
+        # 字典加入list
+        list_data.append(cases)
+    return list_data
+
+
+def creatFile(file_title):
+    file_title_s = file_title
+    with open(f'file/{file_title_s}.csv', mode='a', encoding='utf-8-sig', newline='') as f:
+        print("新建", file_title, "成功")
         csv_write = csv.writer(f)
         csv_write.writerow(['地区', '日期', '时刻', '坝前水位', '入库流量', '出库'])
-    # 当前时间：
+
+
+def saveData(file_title, RECORDDATE, RECORDQUARTER, WLUPSTREAM, WLDOWNSTREAM, FLOWOUT):
+    file_title_s = file_title
+    RECORDDATE_s = RECORDDATE
+    RECORDQUARTER_s = RECORDQUARTER
+    WLUPSTREAM_s = WLUPSTREAM
+    WLDOWNSTREAM_s = WLDOWNSTREAM
+    FLOWOUT_s = FLOWOUT
+    with open(f'file/{file_title_s}.csv', mode='a', encoding='utf-8-sig', newline='') as f:
+        csv_write = csv.writer(f)
+        csv_write.writerow([file_title_s, RECORDDATE_s, RECORDQUARTER_s, WLUPSTREAM_s, WLDOWNSTREAM_s, FLOWOUT_s])
+
+list_data_s = get_region()
+    # 地区循环
+for data in list_data_s:
+    file_title = data['name']
+    # 创建及编写表头文件
+    creatFile(file_title)
     begin = datetime.date(2019, 1, 1)
     end = datetime.date(2021, 12, 31)
     d = begin
@@ -62,13 +83,12 @@ for data in list_data:
         # 当天时刻循环
         try:
             for li in datas:
+                RECORDDATE = li['RECORDDATE']
                 RECORDQUARTER = li['RECORDQUARTER']
                 WLUPSTREAM = li['WLUPSTREAM']
                 WLDOWNSTREAM = li['WLDOWNSTREAM']
                 FLOWOUT = li['FLOWOUT']
-                with open(f'file/{file_title}.csv', mode='a', encoding='utf-8-sig', newline='') as f:
-                    csv_write = csv.writer(f)
-                    csv_write.writerow([file_title, date_f, RECORDQUARTER, WLUPSTREAM, WLDOWNSTREAM, FLOWOUT])
+                saveData([file_title, RECORDDATE, RECORDQUARTER, WLUPSTREAM, WLDOWNSTREAM, FLOWOUT])
         except Exception as e:
             print(e)
         d += delta
