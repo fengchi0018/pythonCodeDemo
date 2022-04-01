bac65c55a08631e472660415bef3afd711201e60
fengchi0018 <2652892843@qq.com>
2022-04-01 17:57:16 +0800

    0401

---
+++ python_35/read_yaml.py
@@ -0,0 +1,17 @@
+''''
+@File  : read_yaml.py
+@Author: Feng
+@Date  : 2022/4/1
+@Desc  : 
+'''
+
+import yaml
+f = open('test.yaml','r',encoding='utf-8')
+data = f.read()
+f.close()
+all_data = yaml.load_all(data,Loader=yaml.FullLoader)
+for data in all_data:
+    # data字典
+    print(data)
+    print(data['address'][0]['street'])
+
